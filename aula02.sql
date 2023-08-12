fcc8c97978d8234dd5a49e6951f205ead13e3d9b
Marcelo Torrieli <marcelinhonet@hotmail.com>
2023-08-11 22:26:58 -0300

    Update aula02.sql

---
+++ aula02.sql
@@ -0,0 +1,7 @@
+create database bd_sistema_bancario;
+\c bd_sistema_bancario;
+create table tabela01 (codigo integer);
+create table tabela02 (codigo integer, nome text, telefone integer, endereco varchar);
+create table tabela03 (codigo integer, ra integer);
+create table tabela04 (codigo integer, email varchar);
+create table tabela05 (codigo integer, idade integer);
